03cfee3ccb05b64fd05fe95b01ec41000bb1cbb5
Vansh Chaudhary <vansh28900@gmail.com>
2026-04-06 23:44:31 +0530

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    unordered_map<int,int> m;
+    int helper(vector<int>& coins, int amount) {
+        if(amount == 0) return 0;
+        if(amount<0) return INT_MAX;
+
+        if(m.find(amount) != m.end()) return m[amount];
+
+        int res = INT_MAX-1;
+        for(auto &i: coins) {
+            if(i<=amount) {
+                res  = min(res, helper(coins, amount-i)+1);
+            } else break;
+        }
+
+        return m[amount] = res;
+
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        sort(coins.begin(), coins.end());
+        int res = helper(coins, amount);
+        if(res == INT_MAX-1) return -1;
+        return res;
+    }
+};
