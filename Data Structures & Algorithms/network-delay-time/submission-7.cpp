be69be593af367e9677db01053a0f39859427737
Vansh Chaudhary <vansh28900@gmail.com>
2026-05-23 11:17:53 +0530

    Add: network-delay-time - submission-7

---
+++ Data Structures & Algorithms/network-delay-time/submission-7.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+        vector<vector<pair<int,int>>> adj(n+1);
+        for(auto i: times) {
+            int dist = i[2];
+            int node = i[1];
+            adj[i[0]].emplace_back(make_pair(dist, node));
+        }
+
+        vector<int> dist(n+1, INT_MAX);
+        dist[k] = 0;
+        dist[0] = 0;
+
+        pq.push({0,k});
+
+        while(!pq.empty()) {
+            auto i = pq.top();
+            pq.pop();
+            int oldDist = i.first;
+            for(auto &neigh: adj[i.second]) {
+                int newDist = neigh.first;
+                if(dist[neigh.second] > oldDist+newDist) {
+                    dist[neigh.second] = oldDist+newDist;
+                    pq.push(make_pair(oldDist+newDist, neigh.second));
+                }
+            }
+        }
+        int res = -1;
+        for(auto i: dist) {
+            if(i == INT_MAX) return -1;
+            res = max(res, i);
+        }
+        return res;
+    }
+};
