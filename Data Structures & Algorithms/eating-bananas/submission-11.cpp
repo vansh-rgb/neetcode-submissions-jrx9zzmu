1ac16349318125a22d615686466bf29bc7c078bc
Vansh Chaudhary <vansh28900@gmail.com>
2026-06-03 16:56:37 +0530

    Add: eating-bananas - submission-11

---
+++ Data Structures & Algorithms/eating-bananas/submission-11.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int checkHours(int mid, vector<int> piles) {
+        int count = 0;
+        for(auto i: piles) {
+            if(i%mid == 0) count+=(i/mid);
+            else count+=(i/mid)+1;
+        }
+        return count;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+        int high = *max_element(piles.begin(), piles.end());
+        int mid = 0;
+        while(low<high) {
+            mid = low+(high-low)/2;
+
+            if(checkHours(mid, piles) <= h) {
+                high = mid;
+            } else {
+                low = mid+1;
+            }
+        }
+        return high;
+    }
+};
